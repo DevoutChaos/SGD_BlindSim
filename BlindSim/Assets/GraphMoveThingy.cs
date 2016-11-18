d60ce1f798c521656993d9e59322c68b3b131295
Liam Faulds <chaosend50@gmail.com>
2016-11-18 00:54:10 +0000

    Initialised and added "GraphMoveThingy.cs"
    GrpahMoveThingy.cs
    -Set up to allow swapping between Game Objects.
    -First object must be set as enabled, with all others disabled.
    --Can enable later objects in sequence from Unity to facilitate testing.
    -Additional parameter can (and will) be added to each Game Object

---
+++ BlindSim/Assets/GraphMoveThingy.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphMoveThingy : MonoBehaviour {
+    //Declarations
+    public GameObject moveRight;
+    public GameObject moveLeft;
+    public GameObject moveDown;
+    public GameObject moveUp;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            moveRight.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moveLeft.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moveUp.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moveDown.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
